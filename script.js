aefa6f5a5173c0ed601b727febf3d88d7319d443
FernandoBlanco10 <blanco.alvarez.moy2002@gmail.com>
2025-06-30 16:04:19 -0600

    Agregue apartado de vídeo y reseñas con carusel

---
+++ script.js
@@ -0,0 +1,27 @@
+const res = document.querySelectorAll('.res');
+const prevBtn = document.querySelector('.prev-btn'); 
+const nextBtn = document.querySelector('.next-btn');
+
+let currentIndex = 0;
+
+function showReview(index) {
+  res.forEach((res, i) => {
+    res.classList.toggle('activa', i === index);
+  });
+}
+
+prevBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + res.length) % res.length;
+  showReview(currentIndex);
+});
+
+nextBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % res.length;
+  showReview(currentIndex);
+});
+
+// Autoplay
+setInterval(() => {
+  currentIndex = (currentIndex + 1) % res.length;
+  showReview(currentIndex);
+}, 5000);
