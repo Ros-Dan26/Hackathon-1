79ed4210cd81bbd4faf645e9fa62c9afd963740b
Daniel_Arias <Rosales_Daniel_A.26@outlook.com>
2025-06-30 17:21:23 -0600

    Actualizacion destacados

--- script.js
+++ script.js
@@ -35,3 +35,14 @@ setInterval(() => {
   currentIndex = (currentIndex + 1) % res.length;
   showReview(currentIndex);
 }, 5000);
+
+//Botones
+function mostrarContenedor(idMostrar) {
+    document.getElementById('Mujeres').style.display = 'none';
+    document.getElementById('Hombres').style.display = 'none';
+    document.getElementById('Niños').style.display = 'none';
+    document.getElementById('Accesorios').style.display = 'none';
+
+
+    document.getElementById(idMostrar).style.display = 'block';
+}
